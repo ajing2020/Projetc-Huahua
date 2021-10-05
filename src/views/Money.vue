367d302c8667c6be75a515021d179b400e63e487
ajing2020 <1277483876@qq.com>
2021-10-05 16:53:16 +0800

    money页面布局

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,14 +1,27 @@
 <template>
-  <div>money.vue
-  <Nav />
+  <div class="nav-wrapper">
+    <div class="content">
+      <p>money.vue</p>
+    </div>
+    <Nav />
   </div>
 </template>
 
 <script lang="ts">
 export default {
-  name: "Money"
+  name: "Money",
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.nav-wrapper {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid red;
+  height: 100vh;
+}
+.content {
+    overflow: auto;
+  flex-grow: 1;
+}
 </style>
