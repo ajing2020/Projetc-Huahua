398b8aa58d36c3ac2012ce95870489d6b190e97b
ajing2020 <1277483876@qq.com>
2021-10-05 16:36:34 +0800

    添加404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div>您要找的页面不存在，请检查网址是否正确</div>
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
