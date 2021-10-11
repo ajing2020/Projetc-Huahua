3b06387a67566185692a69828a03290906a77070
ajing2020 <1277483876@qq.com>
2021-10-11 10:18:24 +0800

    使用Js实现了组件的一个功能

--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,13 +1,35 @@
 <template>
   <ul class="types">
-    <li class="selected">支出</li>
-    <li>收入</li>
+    <li 
+    :class="type === '-' && 'selected'" 
+    @click="selectType('-')">支出</li>
+    <li 
+    :class="type === '+' && 'selected'"
+    @click="selectType('+')">收入</li>
   </ul>
 </template>
 
 <script>
 export default {
   name: "Types",
+  props:['xxx'],
+  mounted(){
+    console.log(this.xxx);
+  },
+  data() {
+    return {
+      type: "-", //-表示支出 +表示收入
+    };
+  },
+  methods: {
+    selectType(type) {
+      //type只能是-或+
+      if (type !== "-" && type !== "+") {
+        throw new Error("type is unkonw");
+      }
+      this.type = type;
+    },
+  },
 };
 </script>
 
