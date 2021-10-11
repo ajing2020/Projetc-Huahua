853063653cd10d8bd62a201313b124320fa59d60
ajing2020 <1277483876@qq.com>
2021-10-11 15:08:44 +0800

    可以选中或取消tag

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,22 +1,16 @@
 <template>
   <label class="notes">
     <span class="name">备注</span>
-    <input
-      type="text"
-      v-model="value"
-      placeholder="请输入备注"
-    />
+    <input type="text" v-model="value" placeholder="请输入备注" />
   </label>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from "vue";
 import { Component } from "vue-property-decorator";
-
 @Component
 export default class Notes extends Vue {
   value = "";
-  
 }
 </script>
 
