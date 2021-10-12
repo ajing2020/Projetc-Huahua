58962dba19ada3c5be87a835382ed6ed7fb18410
ajing2020 <1277483876@qq.com>
2021-10-12 12:01:18 +0800

    标签页面的展示和创建标签功能实现

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -14,17 +14,17 @@ import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component, Watch } from "vue-property-decorator";
-import model from '@/model'
+import recordListModel from "@/models/recordListModel";
+import tagListModel from "@/models/tagListModel";
 
-
-const recordList= model.fetch
-
+const recordList = recordListModel.fetch();
+const tagList = tagListModel.fetch();
 
 @Component({
   components: { NumberPad, Types, Tags, Notes },
 })
 export default class Money extends Vue {
-  tags = ["衣", "食", "住", "行"];
+  tags = tagList;
   recordList: RecordItem[] = JSON.parse(
     window.localStorage.getItem("recordList") || "[]"
   );
@@ -41,13 +41,13 @@ export default class Money extends Vue {
     this.record.amount = parseFloat(value);
   }
   saveRecord() {
-    const record2: RecordItem = model.clone(this.record)
+    const record2: RecordItem = recordListModel.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
   }
   @Watch("recordList")
   onRecordListChamged() {
-    model.save(this.recordList);
+    recordListModel.save(this.recordList);
   }
 }
 </script>
