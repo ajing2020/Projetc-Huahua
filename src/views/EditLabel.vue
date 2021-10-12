80825f92528f19405cf237f573b01b961d55173e
ajing2020 <1277483876@qq.com>
2021-10-12 13:58:35 +0800

    新增了editLabel组件，同时设置了一下路由可以传参

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -5,12 +5,23 @@
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
+    import tagListModel from '@/models/tagListModel'
+import Vue from 'vue'
     import {Component} from 'vue-property-decorator'
 
     @Component
     export default class EditLabel extends Vue{
-
+        created(){
+            const id = this.$route.params.id
+            tagListModel.fetch();
+            const tags = tagListModel.data
+            const tag = tags.filter(t=>t.id===id)[0];
+            if(tag){
+                console.log(tag);
+            }else{
+                this.$router.replace('/404')
+            }
+        }
     }
     
 </script>
