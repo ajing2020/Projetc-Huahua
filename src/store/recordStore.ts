446023f169d86746b4964e2ce863df82f4e893bd
ajing2020 <1277483876@qq.com>
2021-10-14 10:12:10 +0800

    抽离Tabs.vue，使用在Statistics.vue里面

--- src/store/recordStore.ts
+++ src/store/recordStore.ts
@@ -1,20 +1,20 @@
-import clone from '@/lib/clone'
+// import clone from '@/lib/clone'
 
 
 
-const recordStore = {
-    recordList :[] as RecordItem[],
-    fetchRecords(): RecordItem[] {
-        this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
-        return this.recordList
-    },
-    createRecord(record: RecordItem) {
-        const record2: RecordItem = clone(record);
-        record2.createdAt = new Date();
-         this.recordList?.push(record2);
-        recordStore.saveRecords();
-    }
-}
-recordStore.fetchRecords();
+// const recordStore = {
+//     recordList :[] as RecordItem[],
+//     fetchRecords(): RecordItem[] {
+//         this.recordList = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];
+//         return this.recordList
+//     },
+//     createRecord(record: RecordItem) {
+//         const record2: RecordItem = clone(record);
+//         record2.createdAt = new Date();
+//          this.recordList?.push(record2);
+//         recordStore.saveRecords();
+//     }
+// }
+// recordStore.fetchRecords();
 
-export default recordStore
+// export default recordStore
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,12 +1,42 @@
 <template>
-      <Layout>
-          <p>stctistics.vue</p>
-      </Layout>
+  <Layout>
+    <Tabs classPrefix="type" :dataSource="typeList" :value.sync="type" />
+    <Tabs classPrefix="interval" :dataSource="intervalList" :value.sync="interval" />
+  </Layout>
 </template>
 
-<script lang="ts">
-
-    export default {
-        name:'Statistics'
+<style lang="scss" scoped>
+::v-deep .type-tabs-item {
+  background: #c4c4c4;
+  &.selected {
+    background: white;
+    &::after {
+      display: none;
     }
+  }
+}
+</style>
+
+<script lang="ts">
+import Vue from "vue";
+import { Component } from "vue-property-decorator";
+import Types from "@/components/Money/Types.vue";
+import Tabs from "@/components/Tabs.vue";
+
+@Component({
+  components: { Types, Tabs },
+})
+export default class Statistics extends Vue {
+  type = "-";
+  interval = "day";
+  intervalList = [
+    { text: "按天", value: "day" },
+    { text: "按周", value: "week" },
+    { text: "按月", value: "month" },
+  ];
+  typeList = [
+    { text: "支出", value: "-" },
+    { text: "收入", value: "+" },
+  ];
+}
 </script>
